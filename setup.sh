8d290441b65df567361ce88ad0c1ee1ef1baf20b
Arvind Laxminarayan <ardsrk@gmail.com>
2013-01-24 19:24:39 +0530

    added git-churn

--- setup.sh
+++ setup.sh
@@ -9,11 +9,26 @@ using_git()
   type git > /dev/null 2>&1
 }
 
+create_home_bin()
+{
+  if [ ! -d $HOME/bin ]; then
+    mkdir $HOME/bin
+  fi
+  true
+}
+
+make_bins_executable()
+{
+  for f in `echo $HOME/bin/*`; chmod +x $f
+}
+
 cp dot_zshrc $HOME/.zshrc
 cp dot_ackrc $HOME/.ackrc
 cp dot_pryrc $HOME/.pryrc
 cp dot_irbrc $HOME/.irbrc
 cp dot_tmux_conf $HOME/.tmux.conf
+create_home_bin && cp bin/* $HOME/bin
+make_bins_executable
 
 if using_rvm; then
   chmod +x $rvm_path/hooks/after_cd_bundler
